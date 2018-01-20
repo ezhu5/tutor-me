a4891251b2affcf0dfa388bdc62bde7a1ae581ac
nosup <bryanki97@gmail.com>
2018-01-20 15:30:10 -0500

    chat stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {SocketIoModule, SocketIoConfig } from 'ng-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
+
 
 @NgModule({
   declarations: [
@@ -21,7 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    SocketIoModule.forRoot(config),
+    IonicModule.forRoot(MyApp) 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/message/message.ts
+++ src/pages/message/message.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Socket } from 'ng-socket-io';
 
 @Component({
   selector: 'page-message',
   templateUrl: 'message.html'
 })
 export class MessagePage {
+	//the user name from login ?
+	nickname = '';
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController, private socket: Socket) {
 
-  }
+	}
 
+	joinChat() {
+		this.socket.connect();
+		this.socket.emit('set-nickname', this.nickname);
+		this.navCtrl.push('ChatRoomPage', {nickname: this.nickname});
+	}
+
+ 
 }
