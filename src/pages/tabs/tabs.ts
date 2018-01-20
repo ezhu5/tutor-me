de60f9e02e2bf14efd8c30132bb43495802792ba
ezhu5 <ezhu@jhu.edu>
2018-01-19 23:51:38 -0500

    Added correct tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { SearchPage } from '../search/search';
+import { MessagePage } from '../message/message';
+import { ProfilePage } from '../profile/profile';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = SearchPage;
+  tab2Root = MessagePage;
+  tab3Root = ProfilePage;
 
   constructor() {
 
