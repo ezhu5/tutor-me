136dd0b69bb62dcfd079696cbe782fe3e35dca41
matthewsmkim1 <matthew.sm.kim1@gmail.com>
2018-01-21 03:24:40 -0500

    Got rid of useless import

--- src/pages/userprofile/userprofile.ts
+++ src/pages/userprofile/userprofile.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { UserProfilePage } from '../../models/user-profile.ts';
 import { AngularFireDatabase } from 'angularfire2/database';
 
 /**
